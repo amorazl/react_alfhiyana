712d6ddb4f393be76b9a9b6ba743cfb7b8ac67d0
alfhiyana <107467634+amorazl@users.noreply.github.com>
2023-10-19 17:34:25 +0700

    feat/23_authentication

---
+++ 23_Authentication-React/my-app/src/pages/EditProduct.jsx
@@ -0,0 +1,212 @@
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { Container, Form, Button, Row, Col, FloatingLabel } from "react-bootstrap";
+
+import NavbarApp from "../components/NavbarApp";
+import FooterApp from "../components/FooterApp";
+
+export default function EditProduct() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [errors, setErrors] = useState({});
+
+  const [product, setProduct] = useState({
+    id: "",
+    productName: "",
+    productCategory: "",
+    productImage: null,
+    productFreshness: "",
+    productDesc: "",
+    productPrice: "",
+  });
+
+  const validateProductName = (value) => {
+    if (value.trim() === "") {
+      return "This field is required.";
+    } else if (/[@#$%^&*()_+[\]{}|\\<>?~]/.test(value)) {
+      return "Input contains symbols. Symbols are not allowed.";
+    } else if (value.length === 10) {
+      alert("Last Name must not exceed 25 characters.");
+    } else if (value.length > 25) {
+      return "Input must be 25 characters or less.";
+    }
+    return "";
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value, type } = e.target;
+
+    if (type === "file") {
+      const file = e.target.files[0];
+      setProduct({
+        ...product,
+        [name]: file,
+      });
+    } else {
+      setProduct({
+        ...product,
+        [name]: value,
+      });
+    }
+
+    if (name === "productName") {
+      const errorMessage = validateProductName(value);
+      setErrors({
+        ...errors,
+        productName: errorMessage,
+      });
+    } else {
+      if (value.trim() !== "") {
+        setErrors({
+          ...errors,
+          [name]: "",
+        });
+      }
+    }
+  };
+
+  useEffect(() => {
+    axios
+      .get(`https://652f67620b8d8ddac0b2710c.mockapi.io/product/${id}`)
+      .then((res) => setProduct(res.data))
+      .catch((err) => console.log(err));
+  }, [id]);
+
+  const handleUpdate = (e) => {
+    e.preventDefault();
+
+    axios
+      .put(`https://652f67620b8d8ddac0b2710c.mockapi.io/product/${id}`, product)
+      .then((res) => {
+        console.log("Product updated:", res.data);
+        navigate("/table-product");
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <>
+      <NavbarApp />
+      <Container>
+        <h3 className="my-4 font-semibold text-center">Edit Product</h3>
+        <Row className="pb-5 justify-content-center">
+          <Col md={5}>
+            <h5 className="fw-semibold mt-3">Detail Product</h5>
+            <Form noValidate onSubmit={handleUpdate}>
+              <Form.Group className="mb-4" controlId="productName">
+                <Form.Label>Product Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="productName"
+                  value={product.productName}
+                  onChange={handleInputChange}
+                  isInvalid={!!errors.productName}
+                  required
+                />
+                <Form.Control.Feedback type="invalid">{errors.productName}</Form.Control.Feedback>
+              </Form.Group>
+
+              <Form.Group className="mb-4" controlId="productCategory">
+                <Form.Label>Product Category</Form.Label>
+                <Form.Control
+                  as="select"
+                  name="productCategory"
+                  value={product.productCategory}
+                  onChange={handleInputChange}
+                  isInvalid={!!errors.productCategory}
+                >
+                  <option value="">- - Select an option - -</option>
+                  <option value="Skincare">Skincare</option>
+                  <option value="Makeup">Makeup</option>
+                  <option value="Jewelry">Jewelry</option>
+                </Form.Control>
+                <Form.Control.Feedback type="invalid">{errors.productCategory}</Form.Control.Feedback>
+              </Form.Group>
+
+              <Form.Group className="mb-4" controlId="productImage">
+                <Form.Label>Product Image</Form.Label>
+                <Form.Control
+                  type="file"
+                  name="productImage"
+                  onChange={handleInputChange}
+                  isInvalid={!!errors.productImage}
+                />
+                <Form.Control.Feedback type="invalid">{errors.productImage}</Form.Control.Feedback>
+              </Form.Group>
+
+              <Form.Group className="mb-4" controlId="productFreshness">
+                <Form.Label>Product Freshness</Form.Label>
+                <Form.Check
+                  type="radio"
+                  name="productFreshness"
+                  id="productFreshness01"
+                  checked={product.productFreshness === "Brand New"}
+                  value="Brand New"
+                  label="Brand New"
+                  onChange={handleInputChange}
+                  isInvalid={!!errors.productFreshness}
+                />
+                <Form.Check
+                  type="radio"
+                  name="productFreshness"
+                  id="productFreshness02"
+                  checked={product.productFreshness === "Second Hand"}
+                  value="Second Hand"
+                  label="Second Hand"
+                  onChange={handleInputChange}
+                  isInvalid={!!errors.productFreshness}
+                />
+                <Form.Check
+                  type="radio"
+                  name="productFreshness"
+                  id="productFreshness03"
+                  checked={product.productFreshness === "Refurbished"}
+                  value="Refurbished"
+                  label="Refurbished"
+                  onChange={handleInputChange}
+                  isInvalid={!!errors.productFreshness}
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-4" controlId="productDesc">
+                <FloatingLabel label="Additional Description">
+                  <Form.Control
+                    as="textarea"
+                    placeholder="Comments"
+                    style={{ height: "100px" }}
+                    name="productDesc"
+                    value={product.productDesc}
+                    onChange={handleInputChange}
+                    isInvalid={!!errors.productDesc}
+                  />
+                  <Form.Control.Feedback type="invalid">{errors.productDesc}</Form.Control.Feedback>
+                </FloatingLabel>
+              </Form.Group>
+
+              <Form.Group className="mb-4" controlId="productPrice">
+                <Form.Label>Product Price</Form.Label>
+                <Form.Control
+                  type="number"
+                  name="productPrice"
+                  placeholder="$1"
+                  value={product.productPrice}
+                  onChange={handleInputChange}
+                  isInvalid={!!errors.productPrice}
+                />
+                <Form.Control.Feedback type="invalid">{errors.productPrice}</Form.Control.Feedback>
+              </Form.Group>
+
+              <div className="d-grid gap-2">
+                <Button type="submit" variant="dark">
+                  Update
+                </Button>
+              </div>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+      <FooterApp />
+    </>
+  );
+}
